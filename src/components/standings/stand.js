4cd9d3c06a0efb9024274a05ab887a3581290fe4
visita <visita@MacBooks-MacBook-Pro.local>
2024-11-04 22:08:45 -0400

    Standings: simple css stuff

---
+++ src/components/standings/stand.js
@@ -0,0 +1,184 @@
+import { ref } from "vue"
+
+export const standing = ref([
+    {   
+            logo: 'https://midfield.mlbstatic.com/v1/team/119/spots/72',
+            name: 'LAD',   
+            stats: {
+                W: '90',
+                L: '87',
+                PTC: '.500',
+                WCGB: '-',
+                L10: '90',
+                STRK: '90',
+                RS: '90',
+                RA: '90',
+                DIFF: '90'
+            }
+    },
+    {
+            logo: 'https://midfield.mlbstatic.com/v1/team/110/spots/72',
+            name: 'BAL',
+            stats: {
+                W: '80',
+                L: '77',
+                PTC: '.400',
+                WCGB: '-',
+                L10: '80',
+                STRK: '80',
+                RS: '80',
+                RA: '80',
+                DIFF: '80'
+            }
+    },
+    {   
+            logo: 'https://midfield.mlbstatic.com/v1/team/119/spots/72',
+            name: 'LAD',   
+            stats: {
+                W: '90',
+                L: '87',
+                PTC: '.500',
+                WCGB: '-',
+                L10: '90',
+                STRK: '90',
+                RS: '90',
+                RA: '90',
+                DIFF: '90'
+            }
+    },
+    {
+            logo: 'https://midfield.mlbstatic.com/v1/team/110/spots/72',
+            name: 'BAL',
+            stats: {
+                W: '80',
+                L: '77',
+                PTC: '.400',
+                WCGB: '-',
+                L10: '80',
+                STRK: '80',
+                RS: '80',
+                RA: '80',
+                DIFF: '80'
+            }
+    },
+    {   
+            logo: 'https://midfield.mlbstatic.com/v1/team/119/spots/72',
+            name: 'LAD',   
+            stats: {
+                W: '90',
+                L: '87',
+                PTC: '.500',
+                WCGB: '-',
+                L10: '90',
+                STRK: '90',
+                RS: '90',
+                RA: '90',
+                DIFF: '90'
+            }
+    },
+    {
+            logo: 'https://midfield.mlbstatic.com/v1/team/110/spots/72',
+            name: 'BAL',
+            stats: {
+                W: '80',
+                L: '77',
+                PTC: '.400',
+                WCGB: '-',
+                L10: '80',
+                STRK: '80',
+                RS: '80',
+                RA: '80',
+                DIFF: '80'
+            }
+    },
+    {   
+            logo: 'https://midfield.mlbstatic.com/v1/team/119/spots/72',
+            name: 'LAD',   
+            stats: {
+                W: '90',
+                L: '87',
+                PTC: '.500',
+                WCGB: '-',
+                L10: '90',
+                STRK: '90',
+                RS: '90',
+                RA: '90',
+                DIFF: '90'
+            }
+    },
+    {
+            logo: 'https://midfield.mlbstatic.com/v1/team/110/spots/72',
+            name: 'BAL',
+            stats: {
+                W: '80',
+                L: '77',
+                PTC: '.400',
+                WCGB: '-',
+                L10: '80',
+                STRK: '80',
+                RS: '80',
+                RA: '80',
+                DIFF: '80'
+            }
+    },
+    {   
+            logo: 'https://midfield.mlbstatic.com/v1/team/119/spots/72',
+            name: 'LAD',   
+            stats: {
+                W: '90',
+                L: '87',
+                PTC: '.500',
+                WCGB: '-',
+                L10: '90',
+                STRK: '90',
+                RS: '90',
+                RA: '90',
+                DIFF: '90'
+            }
+    },
+    {
+            logo: 'https://midfield.mlbstatic.com/v1/team/110/spots/72',
+            name: 'BAL',
+            stats: {
+                W: '80',
+                L: '77',
+                PTC: '.400',
+                WCGB: '-',
+                L10: '80',
+                STRK: '80',
+                RS: '80',
+                RA: '80',
+                DIFF: '80'
+            }
+    },
+    {   
+            logo: 'https://midfield.mlbstatic.com/v1/team/119/spots/72',
+            name: 'LAD',   
+            stats: {
+                W: '90',
+                L: '87',
+                PTC: '.500',
+                WCGB: '-',
+                L10: '90',
+                STRK: '90',
+                RS: '90',
+                RA: '90',
+                DIFF: '90'
+            }
+    },
+    {
+            logo: 'https://midfield.mlbstatic.com/v1/team/110/spots/72',
+            name: 'BAL',
+            stats: {
+                W: '80',
+                L: '77',
+                PTC: '.400',
+                WCGB: '-',
+                L10: '80',
+                STRK: '80',
+                RS: '80',
+                RA: '80',
+                DIFF: '80'
+            }
+    },
+])
--- src/views/Standings.vue
+++ src/views/Standings.vue
@@ -2,43 +2,7 @@
 import YearPicker from '../components/YearPicker.vue';
 import StandingsTable from '../components/standings/StandingsTable.vue';
 
-import { ref } from "vue"
-
-const standing = ref(null)
-
-standing.value = [
-    {   
-            logo: 'https://midfield.mlbstatic.com/v1/team/119/spots/72',
-            name: 'LAD',   
-            stats: {
-                W: '90',
-                L: '87',
-                PTC: '.500',
-                WCGB: '-',
-                L10: '90',
-                STRK: '90',
-                RS: '90',
-                RA: '90',
-                DIFF: '90'
-            }
-    },
-    {
-            logo: 'https://midfield.mlbstatic.com/v1/team/110/spots/72',
-            name: 'BAL',
-            stats: {
-                W: '80',
-                L: '77',
-                PTC: '.400',
-                WCGB: '-',
-                L10: '80',
-                STRK: '80',
-                RS: '80',
-                RA: '80',
-                DIFF: '80'
-            }
-    }
-]
-
+import { standing } from "../components/standings/stand.js"
 </script>
 
 <template>
@@ -49,8 +13,8 @@ standing.value = [
             <div class="wrap-table-left-side">
                 <StandingsTable class="table-left-side">
                     <template #thead>
-                        <th class="left-table-head">AL LEADERS</th>
-                        <th v-for="(stand, index) in standing" class="left-table-head" >
+                        <th class="left-table-head info-label-table">AL LEADERS</th>
+                        <th v-for="(stand, index) in standing" class="left-table-head main-left-th" >
                             <div class="logo-team-img-wrap">
                                 <img :src="stand.logo" alt="">
                             </div>
@@ -61,33 +25,33 @@ standing.value = [
             </div>
             
             <div class="wrap-table-right-side">
-            <StandingsTable class="table-right-side">
-                <template #thead>
-                    <th>W</th>
-                    <th>L</th>
-                    <th>PTC</th>
-                    <th>WCGB</th>
-                    <th>L10</th>
-                    <th>STRK</th>
-                    <th>RS</th>
-                    <th>RA</th>
-                    <th>DIFF</th>
-                </template>
-                <template #tbody>
-                    <tr v-for="stand in standing">
-                        <td>{{stand.stats.W}}</td>
-                        <td>{{stand.stats.L}}</td>
-                        <td>{{stand.stats.PTC}}</td>
-                        <td>{{stand.stats.WCGB}}</td>
-                        <td>{{stand.stats.L10}}</td>
-                        <td>{{stand.stats.STRK}}</td>
-                        <td>{{stand.stats.RS}}</td>
-                        <td>{{stand.stats.RA}}</td>
-                        <td>{{stand.stats.DIFF}}</td>
-                    </tr>
-                </template>
-            </StandingsTable>
-        </div>
+                <StandingsTable class="table-right-side">
+                    <template #thead>
+                        <th>W</th>
+                        <th>L</th>
+                        <th>PTC</th>
+                        <th>WCGB</th>
+                        <th>L10</th>
+                        <th>STRK</th>
+                        <th>RS</th>
+                        <th>RA</th>
+                        <th>DIFF</th>
+                    </template>
+                    <template #tbody>
+                        <tr v-for="stand in standing">
+                            <td>{{stand.stats.W}}</td>
+                            <td>{{stand.stats.L}}</td>
+                            <td>{{stand.stats.PTC}}</td>
+                            <td>{{stand.stats.WCGB}}</td>
+                            <td>{{stand.stats.L10}}</td>
+                            <td>{{stand.stats.STRK}}</td>
+                            <td>{{stand.stats.RS}}</td>
+                            <td>{{stand.stats.RA}}</td>
+                            <td>{{stand.stats.DIFF}}</td>
+                        </tr>
+                    </template>
+                </StandingsTable>
+            </div>
         </div>
     </div>
 </template>
@@ -98,21 +62,18 @@ standing.value = [
     width: 100%;
 
     margin-top: 59px;
-
-    /* border: 1px solid red; */
-    padding-top: 10px;
+    background-color: #FFF;
+    border-bottom: 2px solid #F3F3F3;
 }
 
 /* left */
 .wrap-table-left-side {
     min-width: 90px;
-
-    /* border: 1px solid red; */
 }
 
 .left-table-head:first-child {
     font-size: 10px;
-    padding: 0px 14px;
+    padding: 10px 14px;
     white-space: nowrap;
 }
 .left-table-head {
@@ -121,25 +82,25 @@ standing.value = [
     align-items: center;
 
     font-size: 14px;
-    padding: 11px 14px;
+}
+
+.main-left-th, .table-right-side td {
+    height: 55px;
+}
+.table-right-side th {
+    padding: 10px 0;
 }
 
 .left-table-head .logo-team-img-wrap {
     margin-right: 10px;
 }
 
-.left-table-head, .table-right-side tr, .table-right-side th {
-    border-bottom: 1px solid black;
-}
 /* right */
 .wrap-table-right-side {
     overflow: scroll;
     white-space: nowrap;
 
-    background-color: white;
-
-    /* border: 1px solid red; */
-    /* margin-top: px; */
+    /* background-color: white; */
 }
 .table-right-side {
     display: inline;
@@ -150,25 +111,25 @@ standing.value = [
 
 .table-right-side td {
     font-size: 14px;
-    padding: 12.5px 18px;
+    padding: 0 18px;
+}
+.table-right-side tr, .main-left-th {
+    border-top: 2px solid #F3F3F3;
 }
 
 .table-right-side tr:hover {
     background-color: rgb(243, 243, 243);
 }
-.table-left-side th:hover {
-    background-color: rgb(243, 243, 243);
-}
 
 th {
     font-size: 10px;
-    /* font-size: 14px; */
 }
 
 .logo-team-img-wrap, .logo-team-img-wrap img {
     height: 24px;
     width: 24px;
 }
+
 
 /* Hide scrollbar for Chrome, Safari and Opera */
 .wrap-table-right-side::-webkit-scrollbar {
